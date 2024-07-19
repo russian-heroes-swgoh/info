76911c06168fd40c016d70964fbf679852d43594
DS211211 <156601140+DS211211@users.noreply.github.com>
2024-07-19 15:59:04 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import guildsInfo from './mock-data/guilds.json';
 import logo from './assets/rh-logo.png';
 import reva from './assets/tex.charui_thirdsister.png';
-import raidPursuit from './assets/Naboo.png'
+import raidNaboo from './assets/Naboo.png'
 import raidPursuit from './assets/raid-pursuit.png'
 import raidKrayt from './assets/raid-krayt.png'
 import raidTriumvirate from './assets/raid-the-sith-triumvirate.png'
