5090072e18787e232d7543665f59d8046d48c8b1
DS211211 <156601140+DS211211@users.noreply.github.com>
2024-07-20 20:08:32 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import guildsInfo from './mock-data/guilds.json';
-import logo from './assets/CIS 4.png'
+import logo from './assets/CIS 4.jpg'
 import raidNaboo from './assets/raid-naboo.png'
 import raidPursuit from './assets/raid-pursuit.png'
 import raidKrayt from './assets/raid-krayt.png'
