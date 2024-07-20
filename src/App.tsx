c50896ab486fe1ff6ad0a0fb3975064d1d01bd3c
DS211211 <156601140+DS211211@users.noreply.github.com>
2024-07-20 19:37:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import guildsInfo from './mock-data/guilds.json';
+import logo from './assets/CIS-2.png'
 import raidNaboo from './assets/raid-naboo.png'
 import raidPursuit from './assets/raid-pursuit.png'
 import raidKrayt from './assets/raid-krayt.png'
